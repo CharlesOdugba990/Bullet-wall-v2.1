228fb159eba2570b4e77ae7c3464836be3aeae54
CharlesOdugba990 <70246468+CharlesOdugba990@users.noreply.github.com>
2020-10-30 21:36:41 -0400

    Add files via upload

---
+++ p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,45 @@
+var bullet,thickness;
+var wall,speed,weight; 
+
+
+function setup() {
+  createCanvas(1600,400);
+  car = createSprite(50, 200, 50, 50);
+  wall = createSprite(1500,200,thickness,height/2)
+  wall.shapeColor = color (80,80,80)
+  speed = random(55,90)
+  weight = random(400,1500)
+  car.velocityX = speed;
+  thickness=random(22,83)
+  speed = random(223,321)
+  weight = random(30,52)
+}
+
+function draw() {
+  background(0);  
+  drawSprites();
+  if(wall.x-car.x<(car.width+wall.width)/2)
+  {
+    car.velocityX = 0;
+    var deformation = (0.5* weight * speed * speed)/22500
+    if(deformation>180)
+    wall.shapeColor = color(255,0,0)
+
+    if(deformation<180 && deformation>100)
+    wall.shapeColor = color(230,230,0)
+
+    if(deformation<100)
+    wall.shapeColor = color(0,255,0)
+  }
+}
+
+
+function hasClilided(Lbullet,Lwall){
+  bulletRightEdge = Lbullet.x + Lbullet.width;
+  wallLeftEdge=Lwall.x;
+  if (bulletRightEdge>=wallLeftEdge){
+    return true
+  }
+  return false;
+}
+
